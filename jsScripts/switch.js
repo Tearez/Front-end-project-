0edf5a805a32c6ee5bb4753a2e8141bf2ec7689d
Tearez <martinreff1@gmail.com>
2018-07-01 00:56:03 +0300

    Added Jquery functionality for tabs switching

--- jsScripts/switch.js
+++ jsScripts/switch.js
@@ -1,4 +1,14 @@
-var switchFunction = function(){
-    var $current = $(this);
-    c
-}
+jQuery(document).ready(function() {
+	jQuery('.navbar-default .navbar-nav li a').on('click', function(e) {
+
+		var currentAttrValue = jQuery(this).attr('href');
+		// Show/Hide Tabs
+		jQuery('.tabs>' + currentAttrValue).siblings().slideUp(600);
+jQuery('.tabs>' + currentAttrValue).delay(600).slideDown(600);
+
+		// Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
+});
