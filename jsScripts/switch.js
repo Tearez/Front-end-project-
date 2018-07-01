16b002a9736313d63b707b7a163b29044e6a2abf
Tearez <martinreff1@gmail.com>
2018-07-01 16:42:56 +0300

    added signup functionality (not really functioning tho)

--- jsScripts/switch.js
+++ jsScripts/switch.js
@@ -3,8 +3,8 @@ jQuery(document).ready(function() {
 
 		var currentAttrValue = jQuery(this).attr('href');
 		// Show/Hide Tabs
-		jQuery('.tabs>' + currentAttrValue).siblings().slideUp(600);
-jQuery('.tabs>' + currentAttrValue).delay(600).slideDown(600);
+		jQuery('.tabs ' + currentAttrValue).siblings().slideUp(600);
+		jQuery('.tabs ' + currentAttrValue).delay(600).slideDown(600);
 
 		// Change/remove current tab to active
 		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
