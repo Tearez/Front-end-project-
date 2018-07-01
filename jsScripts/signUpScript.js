348a7ade52d7279cee9d58f31a9979ccb17184c5
Tearez <martinreff1@gmail.com>
2018-07-01 17:41:49 +0300

    Beautified sign up form and and properly named the .css file

--- jsScripts/signUpScript.js
+++ jsScripts/signUpScript.js
@@ -1,49 +1,50 @@
-var $password = $("#password");
-var $confirmPassword = $("#confirm_password")
-//Hide hints
-$("#signup form span").hide();
+jQuery(document).ready(function () {
+    var $password = $("#password");
+    var $confirmPassword = $("#confirm_password")
+    //Hide hints
+        $("#signup form span").hide();
 
-function isPasswordValid () {
-  return $password.val().length > 8;
-} 
+        function isPasswordValid() {
+            return $password.val().length > 8;
+        }
 
-function arePasswordsMatching () {
-  return $confirmPassword.val() === $password.val();
-} 
+        function arePasswordsMatching() {
+            return $confirmPassword.val() === $password.val();
+        }
 
-function canSubmit() {
-  return isPasswordValid() && arePasswordsMatching();
-} 
+        function canSubmit() {
+            return isPasswordValid() && arePasswordsMatching();
+        }
 
-function passwordEvent(){
-    //Find out if password is valid  
-    if(isPasswordValid()) {
-      //Hide hint if valid
-      $password.next().hide("slow");
-    } else {
-      //else show hint
-      $password.next().show("slow");
-    }
-}
+        function passwordEvent() {
+            //Find out if password is valid  
+            if (isPasswordValid()) {
+                //Hide hint if valid
+                $password.next().hide("slow");
+            } else {
+                //else show hint
+                $password.next().show("slow");
+            }
+        }
 
-function confirmPasswordEvent(){
-    //Find out if password is valid  
-    if(arePasswordsMatching()) {
-      //Hide hint if valid
-      $confirmPassword.next().hide("slow");
-    } else {
-      //else show hint
-      $confirmPassword.next().show("slow");
-    }
-}
-function enableSubmitEvent(){
-  
-  $("#signup #submit").prop("disabled", !canSubmit());
+        function confirmPasswordEvent() {
+            //Find out if password is valid  
+            if (arePasswordsMatching()) {
+                //Hide hint if valid
+                $confirmPassword.next().hide("slow");
+            } else {
+                //else show hint
+                $confirmPassword.next().show("slow");
+            }
+        }
 
-}
-//When event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-enableSubmitEvent();
+        function enableSubmitEvent() {
 
- 
+            $("#signup #submit").prop("disabled", !canSubmit());
+
+        }
+        //When event happens on password input
+        $password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+        $confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+        enableSubmitEvent();
+});
