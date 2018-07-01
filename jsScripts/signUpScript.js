aafb030ac74b3391b71170efdf4dda0f881ef56f
Tearez <martinreff1@gmail.com>
2018-07-01 22:16:20 +0300

    login and logout functionality done form for login complete

--- jsScripts/signUpScript.js
+++ jsScripts/signUpScript.js
@@ -1,50 +1,30 @@
 jQuery(document).ready(function () {
+    var $uname = $('#username');
     var $password = $("#password");
-    var $confirmPassword = $("#confirm_password")
     //Hide hints
-        $("#signup form span").hide();
+    $("#login form span").hide();
 
-        function isPasswordValid() {
-            return $password.val().length > 8;
+    function isInputValid() {
+        return $password.val().length > 8 && $uname.val().length > 4;
+    }
+
+    function passwordEvent() {
+        //Find out if password is valid  
+        if (isInputValid()) {
+            //Hide hint if valid
+            $password.next().hide("slow");
+        } else {
+            //else show hint
+            $password.next().show("slow");
         }
+    }
 
-        function arePasswordsMatching() {
-            return $confirmPassword.val() === $password.val();
-        }
+    function enableSubmitEvent() {
 
-        function canSubmit() {
-            return isPasswordValid() && arePasswordsMatching();
-        }
+        $("#login #submit").prop("disabled", !isInputValid());
 
-        function passwordEvent() {
-            //Find out if password is valid  
-            if (isPasswordValid()) {
-                //Hide hint if valid
-                $password.next().hide("slow");
-            } else {
-                //else show hint
-                $password.next().show("slow");
-            }
-        }
-
-        function confirmPasswordEvent() {
-            //Find out if password is valid  
-            if (arePasswordsMatching()) {
-                //Hide hint if valid
-                $confirmPassword.next().hide("slow");
-            } else {
-                //else show hint
-                $confirmPassword.next().show("slow");
-            }
-        }
-
-        function enableSubmitEvent() {
-
-            $("#signup #submit").prop("disabled", !canSubmit());
-
-        }
-        //When event happens on password input
-        $password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-        $confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
-        enableSubmitEvent();
+    }
+    //When event happens on password input
+    $password.focus(passwordEvent).keyup(passwordEvent).keyup(enableSubmitEvent);
+    enableSubmitEvent();
 });
